1c801b334918780d57e362f7e14106d96d7aac86
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-03-30 12:53:56 +0200

    Put HashRouter instead of BrowserRouter

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Footer from "../components/footer";
 import Navbar from "../components/navigation/navbar";
 import Product from "../components/product/Product";
@@ -11,17 +11,17 @@ import SingleProduct from "../views/single-product";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
-        <Route path={routes.home} element={<Home />} />
+        <Route exact path={routes.home} element={<Home />} />
         <Route path={routes.products} element={<Products />} />
         <Route path={routes.product} element={<SingleProduct />} />
         <Route path={routes.about} element={<AboutUs />} />
         <Route path={routes.contact} element="" />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
